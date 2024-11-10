570a9d2f381f750ecada97a9a13491bbc5bec5ba
Aline Camille <154642478+Linimochi@users.noreply.github.com>
2024-11-10 01:08:59 -0300

    Update crude.sql

--- crude.sql
+++ crude.sql
@@ -9,22 +9,5 @@ CREATE TABLE `formulario` (
   `senha` varchar(45) NOT NULL,
   `endereco` varchar(45) NOT NULL,
   `datan` date NOT NULL,
-  PRIMARY KEY("id")
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Despejando dados para a tabela `formulario`
---
-
-INSERT INTO `formulario` (`id`, `email`, `senha`, `endereco`, `datan`) VALUES
-(0, 'ana@clara.gmail.com', 'anananana', 'sabia 26 ', '2006-04-14'),
-(0, 'ana@clara.gmail.com', 'anananana', 'sabia 26 ', '2006-04-14'),
-(0, 'ana@clara.gmail.com', 'anananana', 'sabia 26 ', '2006-04-14'),
-(0, 'ana@gmail.com', 'aaaa', 'sabia', '0555-05-04'),
-(0, 'ana@gmail.com', 'aaaa', 'sabia', '0555-05-04'),
-(0, 'ana@clara.gmail.com', 'aaa', 'aaaaa', '0000-00-00');
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  PRIMARY KEY(id)
+);
